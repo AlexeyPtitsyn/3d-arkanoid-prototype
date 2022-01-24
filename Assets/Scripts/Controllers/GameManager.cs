15af197e96cb0ac3ef117498f5bc22fc2ba63c4d
Alexey Ptitsyn <alexey.ptitsyn@gmail.com>
2022-01-25 01:18:59 +0300

    Set-up player controls.

--- Assets/Scripts/Controllers/GameManager.cs
+++ Assets/Scripts/Controllers/GameManager.cs
@@ -5,17 +5,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
+namespace Controllers
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    public class GameManager : MonoBehaviour
     {
-        
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
     }
 }
---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEngine.InputSystem.InputAction;
+
+namespace Player
+{
+
+    public class PlayerController : MonoBehaviour
+    {
+        private PlayerControls _controls;
+
+        private void Awake()
+        {
+            _controls = new PlayerControls();
+
+            _controls.GameMap.LaunchBall.performed += OnLaunchBall;
+        }
+
+        private void Update()
+        {
+            var directionPlayer1 = _controls.GameMap.Player1Move.ReadValue<Vector2>();
+            if(directionPlayer1 != Vector2.zero)
+            {
+                var camera1 = GameObject.Find("Player1Camera");
+                camera1.transform.position = directionPlayer1 * Time.deltaTime;
+            }
+
+            var directionPlayer2 = _controls.GameMap.Player2Move.ReadValue<Vector2>();
+            if (directionPlayer2 != Vector2.zero)
+            {
+                var camera2 = GameObject.Find("Player2Camera");
+                camera2.transform.position = directionPlayer2 * Time.deltaTime;
+            }
+        }
+
+        private void OnLaunchBall(CallbackContext context)
+        {
+            Debug.Log("Will launch ball.");
+        }
+
+        private void OnDestroy()
+        {
+            _controls.Dispose();
+        }
+
+        private void OnDisable()
+        {
+            _controls.Disable();
+        }
+
+        private void OnEnable()
+        {
+            _controls.Enable();
+        }
+    }
+}
